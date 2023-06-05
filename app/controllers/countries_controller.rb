9561a6053c121a313c55166a82a48b282cd258e8
Rashaan-Richardson <rashaan.richardson@gmail.com>
2023-06-05 17:38:18 -0600

    pushing the API endpoints

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -1,2 +1,26 @@
 class CountriesController < ApplicationController
+
+    def index
+        countries = Country.all
+        render json: countries
+    end
+
+    def create
+    # Create a new country
+        country = Country.create(country_params)
+        render json: country    
+    end
+
+    
+    def update
+    end
+    
+    def destroy
+    end
+    
+    # Handle strong parameters, so we are secure
+    private
+    def country_params
+        params.require(:country).permit(:name, :age, :hobby, :image)
+    end
 end
